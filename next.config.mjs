72f8519a921ea771e427a3212b4f3487e728848b
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2025-03-12 10:46:16 -0300

    blog

--- next.config.mjs
+++ next.config.mjs
@@ -85,6 +85,32 @@ const nextConfig = (phase) => {
         };
       }
       return config;
+    },
+    output: 'standalone',
+    productionBrowserSourceMaps: false,
+    logging: {
+      level: isDev ? 'debug' : 'error'
+    },
+    headers: async () => {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff'
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'SAMEORIGIN'
+            },
+            {
+              key: 'X-XSS-Protection',
+              value: '1; mode=block'
+            }
+          ],
+        },
+      ]
     }
   };
 };
